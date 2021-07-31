1289b0209062b06d21420dac3b409a7700778eb3
KhitMinnyo <khitminnyo@khitminnyo.com>
2021-07-31 15:02:59 +0630

    Update pwgen.py

--- pwgen.py
+++ pwgen.py
@@ -2,17 +2,6 @@
 #cython: language_level=2
 ##pwgen is Repeated Password Generating tool.
 ##pwgen is developed by KhitMinnyo from AiO.
-#=====================================================================================#
-#                                                                                     #
-#       __/\__  | |/ / |__ (_) |_  |  \/  (_)_ __  _ __  _   _  ___     __/\__        #
-#       \    /  | ' /| '_ \| | __| | |\/| | | '_ \| '_ \| | | |/ _ \    \    /        #
-#       /_  _\  | . \| | | | | |_  | |  | | | | | | | | | |_| | (_) |   /_  _\        #
-#         \/    |_|\_\_| |_|_|\__| |_|  |_|_|_| |_|_| |_|\__, |\___/      \/          #
-#                                                        |___/                        #
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
-#@@@@@@@@@@@@@  Instructor  @@@@@@@@@@@@@@ All in One Hacking Training  @@@@@@@@@@@@@@#
-#*************** Viber 09 976 41 3560 && Mail khitminnyo@khitminnyo.com **************#
-#######################################################################################
 
 #pwgen Version info
 #Version 1.0
@@ -28,16 +17,14 @@ y = input('Enter number of small letters that you want to repeat.  ')
 def UpperToLower():
     for cap in lstCaptial:
         for small in lstSmall:
-                sys.stdout=open("WORDlist.txt","w")
                 print(cap*x + small*y )
-                sys.stdout.close()
+
 
 def LowerToUpper():
     for cap in lstCaptial:
         for small in lstSmall:
-                sys.stdout=open("wordLIST.txt","w")
                 print(small*y + cap*x) 
-                sys.stdout.close()
+
 
 
 flag = True
